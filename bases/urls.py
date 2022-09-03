5e262626814797d3aa04bfca42afe034766c0b50
DeinerSierra <deiner.sierra@gmail.com>
2022-09-02 21:18:40 -0500

    Desarrollo de login completo

--- bases/urls.py
+++ bases/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from bases import views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('',views.Home.as_view(), name='home'),
+    path('login/',auth_views.LoginView.as_view(template_name='bases/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'),name='logout'),
+    path('registro',views.register, name='registro'),
+    
 ]
