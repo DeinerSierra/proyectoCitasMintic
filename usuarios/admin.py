b9eb013e074149cb8f8082f9aaf11b78dd4796d3
Daniel Sanchez <danielitomaniza2000@hotmail.com>
2022-09-06 13:45:10 -0500

    se agrega usuario y paciente

---
+++ usuarios/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from usuarios.models import *
+
+# Register your models here.
+admin.site.register(Historial)
+admin.site.register(Paciente)
+admin.site.register(Rol)
+admin.site.register(Usuario)
+
